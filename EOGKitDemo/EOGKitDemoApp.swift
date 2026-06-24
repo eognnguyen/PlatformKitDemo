4274561a7b7a526bf2f142b8a0609e6d3c480795
nhuteog <Nhut_Nguyen@eogresources.com>
2026-06-24 10:51:47 +0700

    Initial Commit

---
+++ EOGKitDemo/EOGKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EOGKitDemoApp.swift
+//  EOGKitDemo
+//
+//  Created by Nhut Nguyen on 24/6/26.
+//
+
+import SwiftUI
+
+@main
+struct EOGKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
